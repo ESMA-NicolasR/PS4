4c95af12327ebb1786f7db6bf88c0bcf715391d9
ESMA-NicolasR <n.richard@rns.ecolescreatives.com>
2025-04-05 23:33:54 +0200

    [ADD smoother camera

--- Assets/Scripts/Player/CursorMoveCamera.cs
+++ Assets/Scripts/Player/CursorMoveCamera.cs
@@ -1,27 +1,32 @@
-using System.Numerics;
 using UnityEngine;
-using Quaternion = UnityEngine.Quaternion;
-using Vector2 = UnityEngine.Vector2;
 
 public class CursorMoveCamera : MonoBehaviour
 {
+    [Header("Dependencies")]
     public GameObject playerCamera;
-
+    [Header("Constraints")]
     public float maxHorizontalRotation;
     public float maxVerticalRotation;
+    [Header("Tweaking")]
+    public float dampTime;
+    // Internal
+    private Vector3 _rotation;
+    private Vector3 _speed;
     
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
-
     }
 
     void Update()
     {
+        // Deduce target angle from mouse position
         Vector2 mouseScreenPosition = Input.mousePosition;
         Vector2 mouseRelativePosition = new Vector2(mouseScreenPosition.x / Screen.width, mouseScreenPosition.y/Screen.height);
-        float xRotation = Mathf.Lerp(maxVerticalRotation, -maxVerticalRotation, mouseRelativePosition.y);
-        float yRotation = Mathf.Lerp(-maxHorizontalRotation, maxHorizontalRotation, mouseRelativePosition.x);
-        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        float targetXRotation = Mathf.Lerp(maxVerticalRotation, -maxVerticalRotation, mouseRelativePosition.y);
+        float targetYRotation = Mathf.Lerp(-maxHorizontalRotation, maxHorizontalRotation, mouseRelativePosition.x);
+        // Smoothly rotate with damping
+        _rotation = Vector3.SmoothDamp(_rotation, new Vector3(targetXRotation, targetYRotation), ref _speed, dampTime);
+        playerCamera.transform.localRotation = Quaternion.Euler(_rotation);
     }
 }
