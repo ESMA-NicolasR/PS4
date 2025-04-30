c39f404f2c8f4fa5a61d0ca1f751eddade131627
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-04-30 14:34:35 +0200

    [UPD] #13 Cursor Movement

--- Assets/Scripts/Minigames/Asteroides/MinigameAsteroids.cs
+++ Assets/Scripts/Minigames/Asteroides/MinigameAsteroids.cs
@@ -6,27 +6,49 @@ public class MinigameAsteroids : ResourceSystem
 {
     public int minAsteroids;
     public GameObject cursor;
-    public float cursorX;
-    public float cursorY;
+    public float cursorX, cursorY;
+    public int cursorMaxY, cursorMaxX, cursorMinY, cursorMinX;
     public override void ChangeValue(int delta)
     {
         if (delta == 1)//left
         {
-            cursor.transform.localPosition += new Vector3(-0.1f, 0, 0);
+            if (cursorX > cursorMinX)
+            {
+                cursorX -= 1;
+                cursor.transform.localPosition += new Vector3(-0.1f, 0, 0);
+            }
         }
         else if (delta == 2)//right
         {
-            cursor.transform.localPosition += new Vector3(0.1f, 0, 0);
+            if (cursorX < cursorMaxX)
+            {
+                cursorX += 1;
+                cursor.transform.localPosition += new Vector3(0.1f, 0, 0);
+            }
         }
         else if (delta == 3)//up
         {
-            cursor.transform.localPosition += new Vector3(0, 0.1f, 0);
+            if (cursorY < cursorMaxY)
+            {
+                cursorY += 1;
+                cursor.transform.localPosition += new Vector3(0, 0.1f, 0);
+            }
         }
         else if (delta == 4)//down
         {
-            cursor.transform.localPosition += new Vector3(0, -0.1f, 0);
+            if (cursorY > cursorMinY)
+            {
+                cursorY -= 1;
+                cursor.transform.localPosition += new Vector3(0, -0.1f, 0);
+            }
         }
     }
+    
+    public override void SetValue(int newValue)
+    {
+        currentValue = SanitizeValue(newValue);
+    }
+    
     public override void Break()
     {
         SetValue(SanitizeValue(Random.Range(minValue+minAsteroids, maxValue)));
