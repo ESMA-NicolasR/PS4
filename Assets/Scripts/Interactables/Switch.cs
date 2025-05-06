74444a31dc221bfcb7bb5d9cdeaf11765cbf02ab
ESMA-NicolasR <n.richard@rns.ecolescreatives.com>
2025-05-06 10:49:02 +0200

    [ADD] switch interactible

---
+++ Assets/Scripts/Interactables/Switch.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Switch : Clickable
+{
+    private bool _isToggled;
+    public Transform target;
+    public float rotationAmplitude;
+    public ResourceHandle resourceHandle;
+    public int valueOn;
+    public int valueOff;
+
+    protected override void Start()
+    {
+        RotateWithToggle();
+    }
+
+    protected override void Interact()
+    {
+        _isToggled = !_isToggled;
+        RotateWithToggle();
+        UpdateValue();
+    }
+
+    private void RotateWithToggle()
+    {
+        target.eulerAngles = new Vector3(rotationAmplitude*(_isToggled?1:-1), 0, 0);
+    }
+
+    private void UpdateValue()
+    {
+        resourceHandle.SetValue(_isToggled?valueOn:valueOff);
+    }
+}
