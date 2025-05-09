95e858d9fc4178cdf3827624fbd7ea2676323c3b
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-05-09 11:00:12 +0200

    [UPD] #27 Quit Button

--- Assets/Scripts/UI/MenuButtons.cs
+++ Assets/Scripts/UI/MenuButtons.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class MenuButtons : MonoBehaviour
 {
     public void Play()
     {
         SceneManager.LoadScene("AlphaLevel");
+    }
+
+    public void Quit()
+    {
+        EditorApplication.isPlaying = false;
+        Application.Quit();
     }
 }
