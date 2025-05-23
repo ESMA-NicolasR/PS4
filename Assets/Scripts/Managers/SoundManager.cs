da79de940b7a88afb5dbfcff33565a2a74a8dbb5
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-05-23 15:47:19 +0200

    [WIP] #45 Sound System

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,27 +4,10 @@ using System.Collections.Generic;
 
 public class SoundManager : MonoBehaviour
 {
-    public List<AudioClip> sounds = new List<AudioClip>();
-    public List<string> soundNames = new List<string>();
-    public Dictionary<string, AudioClip> soundDictionary = new Dictionary<string, AudioClip>();
     public AudioSource audioSource;
 
-    private void Awake()
+    public void PlaySound(AudioClip sound, float volume = 1.0f)
     {
-        if (sounds.Count == soundNames.Count)
-        {
-            for(int i = 0; i < sounds.Count; i++)
-            {
-                soundDictionary.Add(soundNames[i], sounds[i]);
-            }
-        }
-        
-        //pour le test
-        PlaySound("Teemo Laugh");
-    }
-
-    public void PlaySound(string soundName, float volume = 1.0f)
-    {
-        audioSource.PlayOneShot(soundDictionary[soundName], volume);
+        audioSource.PlayOneShot(sound, volume);
     }
 }
