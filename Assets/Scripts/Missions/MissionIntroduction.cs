2a5fb8bb755ac278ea3324e99f2eea53033a3cd8
ESMA-NicolasR <n.richard@rns.ecolescreatives.com>
2025-05-27 12:31:53 +0200

    [FIX] #57 no light change

--- Assets/Scripts/Missions/MissionIntroduction.cs
+++ Assets/Scripts/Missions/MissionIntroduction.cs
@@ -5,10 +5,6 @@ public class MissionIntroduction : MonoBehaviour
     public ResourceSystemNumber startSystem;
     public ResourceObjectiveData startObjective;
     public MissionManager missionManager;
-    UnityEngine.Rendering.ProbeReferenceVolume probeRefVolume;
-    [Min(1)] public int numberOfCellsBlendedPerFrame = 10;
-    public string scenarioLightsOff;
-    public string scenarioLightsOn;
     
     private void OnEnable()
     {
@@ -22,19 +18,11 @@ public class MissionIntroduction : MonoBehaviour
 
     private void Start()
     {
-        probeRefVolume = UnityEngine.Rendering.ProbeReferenceVolume.instance;
-        probeRefVolume.numberOfCellsBlendedPerFrame = numberOfCellsBlendedPerFrame;
-        
-        // Prepare light scenario
         startSystem.Break(startObjective.targetValue, startObjective.breakValue);
-        probeRefVolume.lightingScenario = scenarioLightsOff;
     }
 
     private void OnSystemChangeValue()
     {
-        // Blend light scenario
-        probeRefVolume.BlendLightingScenario(scenarioLightsOn, Mathf.InverseLerp(startSystem.minValue, startSystem.maxValue, startSystem.currentValue));
-        
         // Check if introduction is complete
         if (startSystem.IsFixed())
         {
